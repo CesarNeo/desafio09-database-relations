f767283a2a3e75184fcb89e33195aa15420059b4
Cesar Emmanuel <71532601+CesarNeo@users.noreply.github.com>
2021-02-04 13:39:38 -0300

    Update 1612385171493-CreateOrdersProducts.ts

--- src/shared/infra/typeorm/migrations/1612385171493-CreateOrdersProducts.ts
+++ src/shared/infra/typeorm/migrations/1612385171493-CreateOrdersProducts.ts
@@ -12,7 +12,7 @@ export default class CreateOrdersProducts1612385171493
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
-            default: 'gen_random_uuid()',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'price',
